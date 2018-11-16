d20194d8bd705ac489164fe3fdca03ecbca399aa
Christian Danscheid <christian.danscheid@outlook.de>
2018-11-16 09:17:48 +0100

    Add type annotations

--- DoubleBufferedCanvas.js
+++ DoubleBufferedCanvas.js
@@ -1,11 +1,32 @@
+/**
+ * Incredibly important documentation goes here.
+ */
 class DoubleBufferedCanvas {
+    /**
+     *
+     * @param {HTMLCanvasElement} canvasElement
+     */
     constructor(canvasElement) {
+        /**
+         * @type {HTMLCanvasElement}
+         */
         this.frontCanvas = canvasElement;
+
+        /**
+         * @type {CanvasRenderingContext2D}
+         * */
         this.frontCanvasContext = canvasElement.getContext('2d');
 
+        /**
+         * @type {HTMLCanvasElement}
+         */
         this.bufferCanvas = document.createElement('canvas');
         this.bufferCanvas.width = canvasElement.width;
         this.bufferCanvas.height = canvasElement.height;
+
+        /**
+         * @type {CanvasRenderingContext2D}
+         */
         this.bufferCanvasContext = this.bufferCanvas.getContext('2d');
     }
 
